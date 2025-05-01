c2fd32ad69c6d508d77527f98ff10a89cc52fab9
exurd <7785022+exurd@users.noreply.github.com>
2025-05-02 00:39:53 +0100

    comment: specify economy api rate limit

--- roblox-discovery.lua
+++ roblox-discovery.lua
@@ -431,7 +431,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     html = read_file(file)
 
     -- economy api start --
-    -- rate limit: 1 minute
+    -- rate limit: 1 minute PER request
     -- ... :)
     -- https://economy.roblox.com/v2/assets/94625279904133/details
     -- roblox-made items can use the assetdelivery api! (for now...?)
